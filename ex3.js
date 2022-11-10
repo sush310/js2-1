9744c970738fc35c9fe8626238b1295ca2a1e9dc
sush <310sush@gmail.com>
2022-11-10 22:56:13 +1100

    Edited

--- ex3.js
+++ ex3.js
@@ -9,3 +9,35 @@
 // 3. Create a function to convert today’s temperature from Celsius to Fahrenheit
 // a) Celsius to Fahrenheit: (°C × 9/5) + 32 = °F
 // b) Fahrenheit to Celsius: (°F − 32) x 5/9 = °C
+
+const date = new Date();
+const d = date.toDateString();
+const time = date.toLocaleTimeString();
+console.log('Date: ' + d);
+console.log('Time: ' + time);
+
+
+let timeout;
+
+function myFunction() {
+  timeout = setTimeout("I'm ready", 5000);
+  return full;
+}
+
+
+
+  const celcius = 27
+  let fahrenheit = (celcius * 9/5) + 32;
+  fahrenheit = Math.floor(fahrenheit);
+  console.log(`The temperature is ${fahrenheit}
+  degrees Fahrenheit.`);
+
+
+
+  let todayCTemp = 25;
+ function checkTemp(){
+    let todayFTemp = (todayCTemp * 9/5) + 32;
+    console.log (todayFTemp + " degrees Farenheit");
+ }
+ checkTemp();
+ 
